b8e3558f32c2278ebee37d77b7290d36ddd5025d
Andrea-Pomati <pomati.andrea@gmail.com>
2023-05-29 18:24:44 +0200

    first commit

---
+++ src/App.vue
@@ -0,0 +1,23 @@
+<script>
+
+export default {
+  name:'App',
+
+  data() {
+    return {
+
+
+    }
+  }
+}
+</script>
+
+<template>
+<div class="container pt-5">
+  <h1>Tutti i progetti del mio portfolio</h1>
+</div>
+</template>
+
+<style scoped>
+
+</style>
