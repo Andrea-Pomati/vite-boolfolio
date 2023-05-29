062c1e3364c8cb0d36a6ea71695bed914badd41c
Andrea-Pomati <pomati.andrea@gmail.com>
2023-05-29 22:14:06 +0200

    feat: added projectCard.vue scaffolding and export

---
+++ src/components/ProjectCard.vue
@@ -0,0 +1,21 @@
+<script>
+export default {
+    name:'ProjectCard',
+
+    data() {
+        return {
+
+        }
+    },
+}
+</script>
+
+<template>
+    <div>
+        Ciao a tutti
+    </div>
+</template>
+
+<style lang="scss" scoped>
+
+</style>
