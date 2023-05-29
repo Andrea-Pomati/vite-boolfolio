7795f8483afa8bd2d7d347a31b3209212f6b3f87
Andrea-Pomati <pomati.andrea@gmail.com>
2023-05-29 19:02:56 +0200

    feat: import axios and methods

--- src/App.vue
+++ src/App.vue
@@ -1,20 +1,41 @@
 <script>
 
+import axios from 'axios';
+
 export default {
   name:'App',
 
   data() {
     return {
 
+      projects: [],
 
     }
-  }
+  },
+
+  created() {
+    this.getProjects();
+  },
+
+  methods: {
+    getProjects() {
+
+      axios.get('http://127.0.0.1:8000/api/projects').then(Response => {
+        console.log(Response);
+      });
+    
+    }
+  },
 }
 </script>
 
 <template>
 <div class="container pt-5">
   <h1>Tutti i progetti del mio portfolio</h1>
+
+  <hr>
+
+
 </div>
 </template>
 
