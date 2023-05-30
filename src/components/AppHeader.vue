466b3093c1337a71f8cd714ba1987f1f965c6aa5
Andrea-Pomati <pomati.andrea@gmail.com>
2023-05-30 18:23:25 +0200

    feat: router-link home

--- src/components/AppHeader.vue
+++ src/components/AppHeader.vue
@@ -15,7 +15,8 @@ export default {
 <div>
     <nav class="navbar navbar-expand-lg bg-body-tertiary">
   <div class="container-fluid">
-    <a class="navbar-brand" href="#">Boolfolio</a>
+    <router-link class="navbar-brand" aria-current="page" :to="{name:'home'}">Boolfolio</router-link>
+
     <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
       <span class="navbar-toggler-icon"></span>
     </button>
