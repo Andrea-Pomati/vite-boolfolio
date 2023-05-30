eec2acdb13c6a7c22ee43d4f825b81830bf82b31
Andrea-Pomati <pomati.andrea@gmail.com>
2023-05-30 16:49:26 +0200

    feat: added AppHome

---
+++ src/pages/AppHome.vue
@@ -0,0 +1,19 @@
+<script>
+export default {
+    name: 'AppHome',
+
+}
+
+</script>
+
+<template>
+
+<div class="container">
+    <h1>Sei nella Homepage</h1>
+</div>
+
+</template>
+
+<style lang="scss" scoped>
+
+</style>
