29e54396ccbaa7b2c76aa366d03c4938be41e980
Andrea-Pomati <pomati.andrea@gmail.com>
2023-05-30 17:32:01 +0200

    feat: added router-link

--- src/pages/AppHome.vue
+++ src/pages/AppHome.vue
@@ -10,6 +10,10 @@ export default {
 
 <div class="container">
     <h1>Sei nella Homepage</h1>
+
+    <hr>
+
+    <router-link :to="{name: 'about'}">Vai alla pagina di about</router-link>
 </div>
 
 </template>
