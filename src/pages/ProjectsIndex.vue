82295c96982132c524be8445860e5774ae3845b0
Andrea-Pomati <pomati.andrea@gmail.com>
2023-05-30 17:55:41 +0200

    feat: added ProjectsIndex

---
+++ src/pages/ProjectsIndex.vue
@@ -0,0 +1,11 @@
+<script>
+export default {
+    name: 'ProjectsIndex',
+}
+</script>
+
+<template>
+    <div class="container">
+        <h1>Tutti i progetti del mio portfolio</h1>
+    </div>
+</template>
