f8156f0dd9e802ece8786da5ec2bf08e38096c91
Andrea-Pomati <pomati.andrea@gmail.com>
2023-05-30 17:31:33 +0200

    feat: about-us path, name and component

--- src/router.js
+++ src/router.js
@@ -1,18 +1,26 @@
 import { createRouter, createWebHistory } from "vue-router";
 
 import AppHome from './pages/AppHome.vue';
+import AppAbout from './pages/AppAbout.vue';
 
 const router = createRouter ({
 
     history: createWebHistory(),
 
     routes: [
-        
+
         {
             path: '/',
             name: 'home',
             component: AppHome,
         },
+
+        {
+            path: '/about-us',
+            name: 'about',
+            component: AppAbout,
+
+        }
     ],
 
 });
