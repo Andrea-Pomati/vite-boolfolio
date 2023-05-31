f9d1a90a6a0ad56984882b73d964906dec73d416
Andrea-Pomati <pomati.andrea@gmail.com>
2023-05-31 17:08:53 +0200

    feat: added return project, methods get project and h1

--- src/pages/SingleProject.vue
+++ src/pages/SingleProject.vue
@@ -1,22 +1,37 @@
 <script>
+import axios from 'axios';
+
 export default {
     name: 'SingleProject',
 
     data() {
         return {
-            projectSlug: ''
+            project: {},
+
+            projectSlug: '',
         }
     },
 
     mounted() {
         console.log(this.$route);
-        this.projectSlug = this.$route.params.slug
+        this.projectSlug = this.$route.params.slug;
+
+        this.getProject();
+    },
+
+    methods: {
+        getProject() {
+            axios.get('http://127.0.0.1:8000/api/projects/' + this.projectSlug).then(response => {
+    
+                this.project = response.data.project;
+            });
+        }
     }
 }
 </script>
 
 <template>
     <div class="container py-5">
-        <h1>Progetto singolo</h1>
+        <h1>{{ project.title }}</h1>
     </div>
 </template>
