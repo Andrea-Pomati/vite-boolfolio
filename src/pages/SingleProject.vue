7c74cf21fc17bf7684ca46c21b3468634d92bb94
Andrea-Pomati <pomati.andrea@gmail.com>
2023-05-31 16:40:35 +0200

    feat: added data and mounted

--- src/pages/SingleProject.vue
+++ src/pages/SingleProject.vue
@@ -1,11 +1,22 @@
 <script>
 export default {
     name: 'SingleProject',
+
+    data() {
+        return {
+            projectSlug: ''
+        }
+    },
+
+    mounted() {
+        console.log(this.$route);
+        this.projectSlug = this.$route.params.slug
+    }
 }
 </script>
 
 <template>
-<div>
-    <h1>Progetto singolo</h1>
-</div>
+    <div class="container py-5">
+        <h1>Progetto singolo</h1>
+    </div>
 </template>
